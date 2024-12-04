dc3bda181f8e5239f530d68f708bcbd7e1ff7276
Mark Noble <mdnoble73@gmail.com>
2024-12-04 14:23:32 -0700

    Setup default files for 25.01

---
+++ code/web/sys/DBMaintenance/version_updates/25.01.00.php
@@ -0,0 +1,34 @@
+<?php
+
+function getUpdates25_01_00(): array {
+	$curTime = time();
+	return [
+		/*'name' => [
+			 'title' => '',
+			 'description' => '',
+			 'continueOnError' => false,
+			 'sql' => [
+				 ''
+			 ]
+		 ], //name*/
+
+		//mark - Grove
+
+		//katherine
+
+		//kirstien
+
+		//kodi
+
+		//alexander - PTFS-Europe
+
+		//chloe - PTFS-Europe
+
+		//James Staub - Nashville Public Library
+
+		//Lucas Montoya - Theke Solutions
+
+		//other
+
+	];
+}

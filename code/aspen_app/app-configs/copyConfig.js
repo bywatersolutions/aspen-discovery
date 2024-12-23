9370b1161f07056f90847f82cde93b7ce12bb2e3
Mark Noble <mdnoble73@gmail.com>
2024-12-23 10:39:33 -0700

    Remove Aspen LiDA from the main Aspen Discovery repository
    Now exists at: https://github.com/Aspen-Discovery/aspen-lida

--- code/aspen_app/app-configs/copyConfig.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-
-fs.copyFile("app.config.js", "ORIGINAL_app.config.js", (err) => {
-	if (err) {
-		return console.log(err);
-	} else {
-		console.log("✅ Copied original config file.")
-	}
-});

c36718bc3e2f63e2caa808ef114662d813ca9d79
Katherine Perdue <Abhyastamita@gmail.com>
2025-01-13 12:10:21 -0500

    Add permissions for native events
    Add permissions to administer event field sets, event types, and native events, view private events, view event reports, and print calendars with headers.  Also add permissions for all locations to opacAdmin role.

--- code/web/sys/DBMaintenance/version_updates/25.02.00.php
+++ code/web/sys/DBMaintenance/version_updates/25.02.00.php
@@ -16,6 +16,30 @@ function getUpdates25_02_00(): array {
 
 
 		//katherine
+		'native_events_permissions' => [
+			'title' => 'Native Events Permissions',
+			'description' => 'Add new permissions for native events',
+			'continueOnError' => true,
+			'sql' => [
+				"INSERT INTO permissions (sectionName, name, requiredModule, weight, description) VALUES ('Web Builder', 'Administer Field Sets', 'Events', 80, 'Allows the user to administer field sets for native events.')",
+				"INSERT INTO permissions (sectionName, name, requiredModule, weight, description) VALUES ('Events', 'Administer Event Types', 'Events', 30, 'Allows the user to administer native event types.')",
+				"INSERT INTO permissions (sectionName, name, requiredModule, weight, description) VALUES ('Events', 'Administer Events for All Locations', 'Events', 40, 'Allows the user to administer native events for all locations.')",
+				"INSERT INTO permissions (sectionName, name, requiredModule, weight, description) VALUES ('Events', 'Administer Events for Home Library Locations', 'Events', 41, 'Allows the user to administer native events for home library locations.')",
+				"INSERT INTO permissions (sectionName, name, requiredModule, weight, description) VALUES ('Events', 'Administer Events for Home Locations', 'Events', 42, 'Allows the user to administer native events for home location.')",
+				"INSERT INTO permissions (sectionName, name, requiredModule, weight, description) VALUES ('Events', 'View Private Events for All Locations', 'Events', 50, 'Allows the user to view private native events for all locations.')",
+				"INSERT INTO permissions (sectionName, name, requiredModule, weight, description) VALUES ('Events', 'View Private Events for Home Library Locations', 'Events', 51, 'Allows the user to view private native events for home library locations.')",
+				"INSERT INTO permissions (sectionName, name, requiredModule, weight, description) VALUES ('Events', 'View Private Events for Home Locations', 'Events', 52, 'Allows the user to view private native events for home location.')",
+				"INSERT INTO permissions (sectionName, name, requiredModule, weight, description) VALUES ('Events', 'View Event Reports for All Libraries', 'Events', 60, 'Allows the user to view event reports for all libraries.')",
+				"INSERT INTO permissions (sectionName, name, requiredModule, weight, description) VALUES ('Events', 'View Event Reports for Home Library', 'Events', 61, 'Allows the user to view event reports for their home library.')",
+				"INSERT INTO permissions (sectionName, name, requiredModule, weight, description) VALUES ('Events', 'Print Calendars with Header Images', 'Events', 70, 'Allows the user to print calendars with header images.')",
+				"INSERT INTO role_permissions(roleId, permissionId) VALUES ((SELECT roleId from roles where name='opacAdmin'), (SELECT id from permissions where name='Administer Field Sets'))",
+				"INSERT INTO role_permissions(roleId, permissionId) VALUES ((SELECT roleId from roles where name='opacAdmin'), (SELECT id from permissions where name='Administer Event Types'))",
+				"INSERT INTO role_permissions(roleId, permissionId) VALUES ((SELECT roleId from roles where name='opacAdmin'), (SELECT id from permissions where name='Administer Events for All Locations'))",
+				"INSERT INTO role_permissions(roleId, permissionId) VALUES ((SELECT roleId from roles where name='opacAdmin'), (SELECT id from permissions where name='View Private Events for All Locations'))",
+				"INSERT INTO role_permissions(roleId, permissionId) VALUES ((SELECT roleId from roles where name='opacAdmin'), (SELECT id from permissions where name='View Event Reports for All Libraries'))",
+				"INSERT INTO role_permissions(roleId, permissionId) VALUES ((SELECT roleId from roles where name='opacAdmin'), (SELECT id from permissions where name='Print Calendars with Header Images'))",
+			],
+		],
 
 		//kirstien - Grove
 

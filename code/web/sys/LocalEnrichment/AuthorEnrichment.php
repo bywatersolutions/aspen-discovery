3cec52bb6f2938ab79efd2f076aaa8074729ba53
LeoStoyanovByWater <leo.stoyanov@bywatersolutions.com>
2025-08-12 14:38:03 -0500

    BWS-PKG - DIS-1210

--- code/web/sys/LocalEnrichment/AuthorEnrichment.php
+++ code/web/sys/LocalEnrichment/AuthorEnrichment.php
@@ -1,4 +1,5 @@
 <?php
+/** @noinspection PhpMissingFieldTypeInspection */
 
 class AuthorEnrichment extends DataObject {
 	public $__table = 'author_enrichment';
@@ -7,44 +8,48 @@ class AuthorEnrichment extends DataObject {
 	public $hideWikipedia;
 	public $wikipediaUrl;
 
+	static ?array $objectStructure = null;
 	static function getObjectStructure($context = ''): array {
-		return [
-			[
-				'property' => 'id',
-				'type' => 'label',
-				'label' => 'Id',
-				'description' => 'The unique id of this setting.',
-				'storeDb' => true,
-			],
-			[
-				'property' => 'authorName',
-				'type' => 'text',
-				'size' => '255',
-				'maxLength' => 255,
-				'label' => 'Author Name',
-				'description' => 'The exact author name used for Wikipedia lookup.',
-				'note' => 'This must match the author\'s name as cleaned and formatted for the Wikipedia API, with any parentheses and their contents removed. When debug mode is enabled for your <a href="/Admin/IPAddresses" target="_blank">IP Address</a>, you can view the exact search term used on the Author page via the displayed debug message.',
-				'storeDb' => true,
-				'required' => true,
-			],
-			[
-				'property' => 'hideWikipedia',
-				'type' => 'checkbox',
-				'label' => 'Hide Wikipedia Information',
-				'description' => 'Whether to hide Wikipedia data for this author.',
-				'storeDb' => true,
-				'required' => false,
-			],
-			[
-				'property' => 'wikipediaUrl',
-				'type' => 'text',
-				'size' => '255',
-				'maxLength' => 255,
-				'label' => 'Wikipedia URL',
-				'description' => 'The URL from which to load Wikipedia data.',
-				'storeDb' => true,
-				'required' => false,
-			],
-		];
+		if (self::$objectStructure == null) {
+			self::$objectStructure = [
+				[
+					'property' => 'id',
+					'type' => 'label',
+					'label' => 'Id',
+					'description' => 'The unique id of this setting.',
+					'storeDb' => true,
+				],
+				[
+					'property' => 'authorName',
+					'type' => 'text',
+					'size' => '255',
+					'maxLength' => 255,
+					'label' => 'Author Name',
+					'description' => 'The exact author name used for Wikipedia lookup.',
+					'note' => 'This must match the author\'s hyperlinked name on the grouped work. When debug mode is enabled for your <a href="/Admin/IPAddresses" target="_blank">IP Address</a> and the Wikipedia lookup has failed, you can view the exact search term used on the Author page via the displayed debug message.',
+					'storeDb' => true,
+					'required' => true,
+				],
+				[
+					'property' => 'hideWikipedia',
+					'type' => 'checkbox',
+					'label' => 'Hide Wikipedia Information',
+					'description' => 'Whether to hide Wikipedia data for this author.',
+					'storeDb' => true,
+					'required' => false,
+				],
+				[
+					'property' => 'wikipediaUrl',
+					'type' => 'text',
+					'size' => '255',
+					'maxLength' => 255,
+					'label' => 'Wikipedia URL',
+					'description' => 'The URL from which to load Wikipedia data.',
+					'storeDb' => true,
+					'required' => false,
+				],
+			];
+		}
+		return self::$objectStructure;
 	}
 }

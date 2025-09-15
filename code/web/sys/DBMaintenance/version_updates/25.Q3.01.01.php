756ab96b812542bb155dec957237238f768e8f32
LeoStoyanovByWater <leo.stoyanov@bywatersolutions.com>
2025-09-15 17:31:38 -0500

    BWS-PKG - Remove RT settings and permissions from roles.

---
+++ code/web/sys/DBMaintenance/version_updates/25.Q3.01.01.php
@@ -0,0 +1,29 @@
+<?php
+function getUpdates25_Q3_01_01(): array {
+	return [
+		/*'name' => [
+			 'title' => '',
+			 'description' => '',
+			 'continueOnError' => false,
+			 'sql' => [
+				 ''
+			 ]
+		 ], //name*/
+
+		'remove_request_tracker_settings' => [
+			'title' => 'Remove Request Tracker Settings & Permissions',
+			'description' => 'Drop the request_tracker_connection table and remove RT permissions.',
+			'continueOnError' => true,
+			'sql' => [
+				'TRUNCATE TABLE request_tracker_connection',
+				'DROP TABLE IF EXISTS request_tracker_connection',
+				'DELETE FROM role_permissions WHERE permissionId IN (SELECT id FROM permissions WHERE name = "Submit Ticket")',
+				'DELETE FROM role_permissions WHERE permissionId IN (SELECT id FROM permissions WHERE name = "View Active Tickets")',
+				'DELETE FROM role_permissions WHERE permissionId IN (SELECT id FROM permissions WHERE name = "Administer Request Tracker Connection")',
+				'DELETE FROM role_permissions WHERE permissionId IN (SELECT id FROM permissions WHERE name = "Set Development Priorities")'
+			]
+		], //remove_request_tracker_settings
+
+
+	];
+}

714e12ca8b861083cd13a0a308479456338eac39
LeoStoyanov <leo.stoyanov24@gmail.com>
2025-10-14 19:29:44 -0500

    RMAINT: DIS-1161 - More options for sorting or filters in Lists.

--- code/web/sys/DBMaintenance/version_updates/25.Q4.00.php
+++ code/web/sys/DBMaintenance/version_updates/25.Q4.00.php
@@ -21,6 +21,15 @@ function getUpdates25_Q4_00(): array {
 				'ALTER TABLE library ADD COLUMN enableThirdPartySMSNotifications TINYINT(1) DEFAULT 0'
 			],
 		], // add_enable_third_party_sms_notifications_option
+		'add_indexes_for_more_user_list_sort_options' => [
+			'title' => 'Add Indexes For More User List Sort Options',
+			'description' => 'Add indexes idx_publicationDateId and idx_callNumberId for faster User List sorting.',
+			'continueOnError' => false,
+			'sql' => [
+				'CREATE INDEX idx_publicationDateId ON grouped_work_records(publicationDateId)',
+				'CREATE INDEX idx_callNumberId ON grouped_work_record_items (callNumberId)'
+			],
+		], // add_indexes_for_more_user_list_sort_options
 
 	];
 }

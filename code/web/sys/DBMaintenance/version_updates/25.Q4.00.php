8f53de057dc2486e916d47e76bef3db97f9cf39e
LeoStoyanov <leo.stoyanov24@gmail.com>
2025-10-14 19:17:11 -0500

    RMAINT: DIS-1352 - Add Third-Party SMS Notification Integration for CarlX Libraries.

--- code/web/sys/DBMaintenance/version_updates/25.Q4.00.php
+++ code/web/sys/DBMaintenance/version_updates/25.Q4.00.php
@@ -12,5 +12,15 @@ function getUpdates25_Q4_00(): array {
 			 ]
 		 ], //name*/
 
+		// Leo Stoyanov - BWS
+		'add_enable_third_party_sms_notifications_option' => [
+			'title' => 'Add "Enable Third Party SMS Notifications" Option',
+			'description' => 'Add "Enable Third Party SMS Notifications" option for CarlX to Library System settings.',
+			'continueOnError' => true,
+			'sql' => [
+				'ALTER TABLE library ADD COLUMN enableThirdPartySMSNotifications TINYINT(1) DEFAULT 0'
+			],
+		], // add_enable_third_party_sms_notifications_option
+
 	];
 }

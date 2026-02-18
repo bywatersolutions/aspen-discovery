f63aa595501d07bc66d3cc7df6223f40330612bd
Jonah Wilson <jonah@Jonahs-MacBook-Pro.local>
2026-02-17 16:03:01 -0800

    Update Event searching so that it won't return a null value.

--- code/web/sys/Recommend/SideFacets.php
+++ code/web/sys/Recommend/SideFacets.php
@@ -59,8 +59,14 @@ class SideFacets implements RecommendationInterface {
 	 * @access private
 	 */
 
-	private function getEventSettings(?LibraryEventsFacetSetting $facetSettings) : ?DataObject {
-		switch ($facetSettings->settingSource) {
+	private function getEventSettings(?LibraryEventsFacetSetting $facetSettings) : DataObject {
+		global $library;
+		$eventSettings = new LibraryEventsSetting();
+		$eventSettings->libraryId = $library->libraryId;
+		
+		// Load eventSettings
+		$eventSettings->find(true);
+		switch ($eventSettings->settingSource) {
 			case 'communico':
 				require_once ROOT_DIR . '/sys/Events/CommunicoSetting.php';
 				$eventSettings = new CommunicoSetting;
@@ -81,7 +87,7 @@ class SideFacets implements RecommendationInterface {
 		
 		$eventSettings->id = $facetSettings->settingId;
 		
-		return $eventSettings->find(true) ? $eventSettings : null;
+		return $eventSettings;
 	}
 
 	/* process

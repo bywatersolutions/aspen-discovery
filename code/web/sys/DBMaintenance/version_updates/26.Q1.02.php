870f06f5d6fc0942aeffc8fbc23a089c66a45553
Yanjun Li <74245023+LiYanjun19@users.noreply.github.com>
2026-02-23 21:26:09 -0500

    RMAINT: release notes and db updates

--- code/web/sys/DBMaintenance/version_updates/26.Q1.02.php
+++ code/web/sys/DBMaintenance/version_updates/26.Q1.02.php
@@ -11,5 +11,13 @@ function getUpdates26_Q1_02(): array {
 				 ''
 			 ]
 		 ], //name*/
+         'require_pin_for_palace_project' => [
+			'title' => 'Add Require PIN for Palace Project Setting',
+			'description' => 'Add Require PIN for Palace Project Setting',
+			'continueOnError' => false,
+			'sql' => [
+				'ALTER TABLE palace_project_settings ADD COLUMN requirePin TINYINT(1) DEFAULT 1',
+			]
+		], //allow_require_pin_for_palace_project
 	];
 }

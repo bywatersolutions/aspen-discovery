53d07039fa82e9ceafc7c6513d8463cc79703115
Jonah Wilson <jonahcwilson@gmail.com>
2026-04-22 20:38:19 -0700

    Add Initial Objects

---
+++ code/overdrive_extract/overdrive_kotlin/OverDriveAPIConnection.kt
@@ -0,0 +1,7 @@
+package com.turning_leaf_technologies.overdrive;
+// This is still a work in progress, but it's something we should break out of the ExtractOverDriveInfo class
+class OverDriveAPIConnection {
+    val apiURL = "https://api.overdrive.com"
+    val version = "v1"
+
+}

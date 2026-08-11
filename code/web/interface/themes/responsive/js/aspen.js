0725c06e4ad834bb5ff40a19f842c7afebeea120
Jonah Wilson <jonah@Jonahs-MacBook-Pro.local>
2026-08-10 17:37:40 -0700

    Update smarty + js

--- code/web/interface/themes/responsive/js/aspen.js
+++ code/web/interface/themes/responsive/js/aspen.js
@@ -14199,30 +14199,45 @@ AspenDiscovery.CollectionSpotlights = (function(){
 			AspenDiscovery.Account.ajaxLightbox(Globals.path + '/Admin/AJAX?method=getAddToSpotlightForm&source=search&id=' + searchId, true);
 			return false;
 		},
-		loadCarousel: function (spotlightListId, titlesUrl){
-			$.getJSON(titlesUrl, function (data) {
-				if (data.success) {
-					//Create an unordered list for display
-					var html = '<ul>';
-					var i = 1;
+		// loadCarousel: function (spotlightListId, titlesUrl){
+		// 	$.getJSON(titlesUrl, function (data) {
+		// 		if (data.success) {
+		// 			//Create an unordered list for display
+		// 			var html = '<ul>';
+		// 			var i = 1;
 
-					$.each(data.titles, function() {
-						html += '<li class="carouselTitleWrapper">' + this.formattedTitle + '</li>';
-						i++;
-					});
+		// 			$.each(data.titles, function() {
+		// 				html += '<li class="carouselTitleWrapper">' + this.formattedTitle + '</li>';
+		// 				i++;
+		// 			});
 
-					html += '</ul>';
+		// 			html += '</ul>';
 
-					var carouselElement = $('#collectionSpotlightCarousel' + spotlightListId);
-					carouselElement.html(html);
-					var jCarousel = carouselElement.jcarousel();
+		// 			var carouselElement = $('#collectionSpotlightCarousel' + spotlightListId);
+		// 			carouselElement.html(html);
+		// 			var jCarousel = carouselElement.jcarousel();
 
-					// Reload carousel
-					jCarousel.jcarousel('reload');
-				} else {
-					AspenDiscovery.showMessage("Error", data.message);
-				}
-			}).fail(AspenDiscovery.ajaxFail);
+		// 			// Reload carousel
+		// 			jCarousel.jcarousel('reload');
+		// 		} else {
+		// 			AspenDiscovery.showMessage("Error", data.message);
+		// 		}
+		// 	}).fail(AspenDiscovery.ajaxFail);
+		// },
+		loadCarouselFromData: function (spotlightListId, data){
+			if (data.success) {
+				var html = '<ul>';
+				$.each(data.titles, function() {
+					html += '<li class="carouselTitleWrapper">' + this.formattedTitle + '</li>';
+				});
+				html += '</ul>';
+
+				var carouselElement = $('#collectionSpotlightCarousel' + spotlightListId);
+				carouselElement.html(html);
+				carouselElement.jcarousel().jcarousel('reload');
+			} else {
+				AspenDiscovery.showMessage("Error", data.message);
+			}
 		},
 		updateSpotlightFields: function () {
 			var collectionSpotlightId = $('#collectionSpotlightId').val();
